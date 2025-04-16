efd7a41766c5ecc82ad8d22fc9563a3c6dcab864
Boglio Simone <bogliosimone@gmail.com>
2025-04-16 19:40:59 +0200

    Switch to toml, scikit-build-core and cMake (#6)
    * Replace setup.py with toml
    * Move from setup.py to scikit-build-core and cMake to build Cython code
    * Improve documentation
    * Remove scipy.sparse._utils dependency

--- similaripy/utility.py
+++ similaripy/utility.py
@@ -5,16 +5,19 @@ VERBOSE = True
 K = 100
 THRESHOLD = 0
 TARGET_ROWS = None # compute all the rows
+_NUM_THREADS = 0 # use all threads
 
 def dot_product(matrix1, matrix2,
     k=K, threshold=THRESHOLD,
     target_rows=TARGET_ROWS,
     verbose=VERBOSE,
-    format_output=FORMAT_OUTPUT
+    format_output=FORMAT_OUTPUT,
+    num_threads=_NUM_THREADS
     ):
     return s_plus(
         matrix1, matrix2=matrix2,
         k=k, threshold=threshold,
         target_rows=target_rows,
         verbose=verbose,
-        format_output=format_output) 
+        format_output=format_output,
+        num_threads=num_threads) 
